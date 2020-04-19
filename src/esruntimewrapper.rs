95b1958b5e56d58bfe27840946b8a45a3b3b652e
Andries Hiemstra <andries@fam-hiemstra.nl>
2020-04-19 11:38:35 +0200

    forget to comment gc_zeal

--- src/esruntimewrapper.rs
+++ src/esruntimewrapper.rs
@@ -204,7 +204,7 @@ pub mod tests {
         rt.do_in_es_runtime_thread_sync(|sm_rt| {
             sm_rt.do_with_jsapi(|_rt, _cx, _global| {
                 // uncomment this to test with gc in sadistic mode
-                crate::es_utils::set_gc_zeal_options(_cx);
+                // crate::es_utils::set_gc_zeal_options(_cx);
             })
         });
 
