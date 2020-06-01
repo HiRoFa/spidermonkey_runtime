59ce11d93520dd6c00f69381e671d7fadef01410
Andries Hiemstra <andries@fam-hiemstra.nl>
2020-06-01 18:59:07 +0200

    see #9 doc test 2 :)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,41 @@
+//!
+//! Welcome to the es_runtime crate
+//!
+//! es_runtime is aimed at making it possible for rust developers to integrate an ECMA-Script engine in their rust projects without having specialized knowledge about ECMA-Script engines.
+//!
+//! The engine used is the Mozilla SpiderMonkey engine (https://developer.mozilla.org/en-US/docs/Mozilla/Projects/SpiderMonkey).
+//!
+//! There are basicly two ways of using this lib
+//!
+//! If you don't feel like using the JSAPI, which comes with quite a manual then you can use the utils in this project which use the EsValueFacade struct to pass variables around.
+//!
+//! There is also number of utils which allow you to use the JSAPI, these can be accessed by calling EsRuntimeWrapper.do_in_es_runtime_thread(_sync)
+//!
+//! # Example
+//!
+//! ```rust
+//!
+//! fn call_jsapi_stuff(rt: &EsRuntimeWrapper) {
+//!     let res = rt.do_in_es_runtime_thread_sync(|sm_rt: &SmRuntimeWrapper| {
+//!     
+//!         // do_with_jsapi does a couple of things
+//!         // 1.  root the global obj
+//!         // 2.   enter the correct Comparment using AutoCompartment        
+//!         sm_rt.do_with_jsapi(|runtime: &Runtime, context: *mut JSContext, global_handle: HandleObject| {
+//!
+//!             // work with JSAPI methods here
+//!             // there are utils in es_utils.rs with examples of working with JSAPI objects and functions.
+//!     
+//!             // return a value
+//!             true
+//!         })
+//!
+//!     });
+//! }
+//!
+//! ```
+//!
+
 #[macro_use]
 extern crate mozjs;
 
